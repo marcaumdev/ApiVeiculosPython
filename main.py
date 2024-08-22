9637942a59220aeaabafb01476030b4bfefeb8dd
marcaumdev <marcaumdev@gmail.com>
2024-08-22 11:29:56 -0300

    api com todas as rotas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from bd import Carros
 
 app = Flask('carros')
@@ -6,3 +6,45 @@ app = Flask('carros')
 @app.route('/carros', methods=['GET'])
 def GetCarros():
     return Carros
+
+@app.route('/carros/<int:id>', methods=['GET'])
+def GetCarro(id):
+    for carro in Carros:
+        if carro['id'] == id:
+            return carro
+    return f"Carro não encontrado!"
+
+@app.route('/carros', methods=['POST'])
+def PostCarro():
+    data = request.get_json()
+    for carro in Carros:
+        if carro['id'] == data['id']:
+            return f"Carro com id {data['id']} já existe!"
+    Carros.append(data)
+    return jsonify(data)
+
+@app.route('/carros', methods=['PUT'])
+def PutCarro():
+    data = request.get_json()
+    for carro in Carros:
+        if carro['id'] == data['id']:
+            if data['ano'] != carro['ano']:
+                carro['ano'] = data['ano']
+            if data['marca'] != carro['marca']:
+                carro['marca'] = data['marca']
+            if data['modelo'] != carro['modelo']:
+                carro['modelo'] = data['modelo']
+            return carro
+    return f"Carro não encontrado!"
+
+@app.route('/carros/<int:id>', methods=['DELETE'])
+def DeleteCarro(id):
+    for carro in Carros:
+        if carro['id'] == id:
+            Carros.remove(carro)
+            return f"Carro com id {id} deletado!"
+    return f"Carro não encontrado!"
+
+
+if __name__ == '__main__':
+    app.run()
